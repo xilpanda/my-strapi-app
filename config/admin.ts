e7596e96d7924c4e54b03c1bae499fb8f0bfb58e
xilpanda <sandro.radinkovic@gmail.com>
2024-12-22 02:38:13 +0100

    Update AUTH_SECRET in admin.ts

--- config/admin.ts
+++ config/admin.ts
@@ -1,6 +1,6 @@
 export default ({ env }) => ({
   auth: {
-    secret: env('ADMIN_JWT_SECRET'),
+    secret: env('AUTH_SECRET', 'd6rmMZv7Qg1zv4aM+bhgpQ=='),
   },
   apiToken: {
     salt: env('API_TOKEN_SALT'),
