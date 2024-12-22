948e00f52a6fa321a1cc4c62ccf596a6777e1cc8
xilpanda <sandro.radinkovic@gmail.com>
2024-12-22 01:03:07 +0100

    Initial commit - Strapi project

---
+++ src/api/user-request/controllers/user-request.ts
@@ -0,0 +1,7 @@
+/**
+ * user-request controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::user-request.user-request');
---
+++ src/api/user-request/services/user-request.ts
@@ -0,0 +1,7 @@
+/**
+ * user-request service
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreService('api::user-request.user-request');
